27f0e12e01958049778b871f1f5917458b2731c6
Krzychu-Lab\Krzychu <dainarudd@gmail.com>
2017-04-22 12:10:55 +0200

    Projekt na develop. Scena z wyborem czegoś na przyciskach.

---
+++ LD38_SmarrWolrd/Assets/Scripts/BuildingSelection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingSelection : MonoBehaviour
+{
+    public List<Button> buildButtons;
+
+
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+
+    }
+
+    public void Selected (Button button)
+    {
+        print (button.GetComponentInChildren<Text>().text);
+    }
+
+
+
+}
