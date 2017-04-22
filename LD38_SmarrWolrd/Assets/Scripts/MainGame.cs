8673f9e69b56f4669aba730a3a4a0de6a24bbdc3
Whigle <i.bro.n.man@gmail.com>
2017-04-22 22:01:38 +0200

    Naprawiłem kamerę, a Marcin nie umiał, taś taś taś taś :D

--- LD38_SmarrWolrd/Assets/Scripts/MainGame.cs
+++ LD38_SmarrWolrd/Assets/Scripts/MainGame.cs
@@ -72,4 +72,25 @@ public class MainGame : MonoBehaviour {
 
     #endregion
 
+    void OnGUI()
+    {
+        Vector3 p = new Vector3();
+        Camera c = Camera.main;
+        Event e = Event.current;
+        Vector2 mousePos = new Vector2();
+
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        mousePos.x = e.mousePosition.x;
+        mousePos.y = c.pixelHeight - e.mousePosition.y;
+
+        p = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, -c.transform.position.z));
+
+        GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+        GUILayout.Label("Screen pixels: " + c.pixelWidth + ":" + c.pixelHeight);
+        GUILayout.Label("Mouse position: " + mousePos);
+        GUILayout.Label("World position: " + p.ToString("F3"));
+        GUILayout.EndArea();
+    }
+
 }
