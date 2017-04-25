b77ebd0c93f6a325e663ad507590f19af1f378c0
ZIEL0N <zielon94@gmail.com>
2017-04-25 14:03:02 +0200

    Labs

--- LD38_SmarrWolrd/Assets/Scripts/SmogScript.cs
+++ LD38_SmarrWolrd/Assets/Scripts/SmogScript.cs
@@ -15,7 +15,7 @@ public class SmogScript : MonoBehaviour
         spwnTime = Time.time;
         direction = Vector3.zero - transform.position;
         direction.Normalize ();
-        transform.position = new Vector3 (transform.position.x, transform.position.y, -0.51f);
+        transform.position = new Vector3 (transform.position.x, transform.position.y, -1.5f);
         transform.rotation = Quaternion.Euler (-180f, 0f, 0f);
         transform.localScale = new Vector3 (ResourcesManager.getResource (Resource.Smog) + 10, ResourcesManager.getResource (Resource.Smog) + 10, 1f);
         startPosition = transform.position;
@@ -37,7 +37,7 @@ public class SmogScript : MonoBehaviour
 
     void OnTriggerEnter (Collider c)
     {
-        if (c.gameObject.tag != "Island")
+        if (c.gameObject.tag != "Island" && c.gameObject.tag != "ChunkIsland" && c.gameObject.tag != "Rtree")
         {
             Destroy (c.gameObject);
         }
@@ -49,13 +49,14 @@ public class SmogScript : MonoBehaviour
 
     void OnCollisionEnter (Collision c)
     {
-        if (c.gameObject.tag != "Island")// && c.gameObject.tag != "ChunkIsland")
+        if (c.gameObject.tag != "Island" && c.gameObject.tag != "ChunkIsland" && c.gameObject.tag != "Rtree")
         {
             Destroy (c.gameObject);
         }
-        //else if (c.gameObject.tag != "Rtree")
-        //{
-        //    Destroy (gameObject);
-        //}
+        else if (c.gameObject.tag == "ChunkIsland")
+        {
+            c.gameObject.GetComponent<IslandField>().fieldType = Building.None;
+        }
+
     }
 }
